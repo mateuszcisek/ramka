394546ffc0c57b11f83c18cb5f93caa6852c9d7a
Mateusz Cisek <cismat@gmail.com>
2022-05-24 18:40:45 +0100

    Tests for the app __call__ method added

---
+++ tests/test_app/test_call.py
@@ -0,0 +1,53 @@
+import tempfile
+from unittest.mock import Mock
+
+from web_framework.app import App
+
+
+def test_app_call_with_static_files_engine():
+    """
+    When the app is initialized with a static files engine
+    And the app is called
+    Then the `_static_files_engine` method is called
+    And the `_wsgi_app` method is not called.
+    """
+    # pylint: disable=protected-access
+
+    environ_mock = Mock()
+    start_response_mock = Mock()
+
+    with tempfile.TemporaryDirectory() as root_dir:
+        mock_static_files_engine = Mock()
+        app = App(
+            root_dir,
+            static_files_dir=root_dir,
+            static_files_engine=mock_static_files_engine,
+        )
+        app._wsgi_app = Mock()
+        app(environ_mock, start_response_mock)
+
+        app._static_files_engine.assert_called_once_with(
+            environ_mock, start_response_mock
+        )
+        app._wsgi_app.assert_not_called()
+
+
+def test_app_call_without_static_files_engine():
+    """
+    When the app is initialized without a static files engine
+    And the app is called
+    Then the `_wsgi_app` method is called
+    And the `_static_files_engine` method is not set.
+    """
+    # pylint: disable=protected-access
+
+    environ_mock = Mock()
+    start_response_mock = Mock()
+
+    with tempfile.TemporaryDirectory() as root_dir:
+        app = App(root_dir)
+        app._wsgi_app = Mock()
+        app(environ_mock, start_response_mock)
+
+        assert not app._static_files_engine
+        app._wsgi_app.assert_called_once_with(environ_mock, start_response_mock)
