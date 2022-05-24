8af25d027a04aa518d0125a94f4d2aa31bc607a7
Mateusz Cisek <cismat@gmail.com>
2022-05-24 18:52:57 +0100

    Tests for the app _wsgi_app method added

---
+++ tests/test_app/test_wsgi_app.py
@@ -0,0 +1,27 @@
+import tempfile
+from unittest.mock import Mock, patch
+
+from web_framework.app import App
+
+
+@patch("web_framework.app.Request")
+def test_wsgi_app_call(mock_request):
+    """
+    When the WSGI application is called
+    Then the request is created and the response is returned.
+    """
+    # pylint: disable=protected-access
+    mock_environ = Mock()
+    mock_start_response = Mock()
+
+    with tempfile.TemporaryDirectory() as root_dir:
+        app = App(root_dir)
+        app._handle_request = Mock()
+
+        app._wsgi_app(mock_environ, mock_start_response)
+
+        mock_request.assert_called_once_with(mock_environ)
+        app._handle_request.assert_called_once_with(mock_request.return_value)
+        app._handle_request.return_value.assert_called_once_with(
+            mock_environ, mock_start_response
+        )
